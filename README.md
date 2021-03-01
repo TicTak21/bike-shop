388392366718cf1721869d31e80b88eca1985b49
Alex <tictak211@gmail.com>
2021-03-01 19:23:50 +0200

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bike-Shop
+
+❤️ [Project template](https://github.com/Jensderond/react-nestjs-template)
---
+++ packages/frontend/src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return <>Frontend</>;
+};
+
+export default App;
