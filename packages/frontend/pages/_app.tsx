113ad162a8a2db92d41687a00f2ee9fbe27d339c
Alex <tictak211@gmail.com>
2021-03-02 11:14:09 +0200

    feat(frontend): chakra-ui and fonts

--- packages/frontend/pages/_app.tsx
+++ packages/frontend/pages/_app.tsx
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from '../styles/chakra/theme';
+import { Fonts } from '../styles/chakra/fonts';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Fonts />
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ packages/frontend/styles/chakra/theme.tsx
@@ -0,0 +1,8 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+  fonts: {
+    heading: "'Futura PT', sans-serif",
+    body: "'Futura PT', sans-serif",
+  },
+});
