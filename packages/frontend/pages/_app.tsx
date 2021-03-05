ed6552998de63658fba8748a861d0ff5deeb1acf
Alex <tictak211@gmail.com>
2021-03-05 19:49:07 +0200

    feat(frontend): ReplainBot support

--- packages/frontend/pages/_app.tsx
+++ packages/frontend/pages/_app.tsx
@@ -10,6 +10,15 @@ function MyApp({ Component, pageProps }) {
       <Fonts />
       <Head>
         <link rel="shortcut icon" href="/favicon/favicon.svg" type="image/x-icon" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.replainSettings = { id: '452ae3fc-bacc-4e2d-85af-fa8066f21679' };
+            (function(u){var s=document.createElement('script');s.type='text/javascript';s.async=true;s.src=u;
+            var x=document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);
+            })('https://widget.replain.cc/dist/client.js');
+            `,
+          }}
+        />
         <title>Streetster</title>
       </Head>
       <Component {...pageProps} />
