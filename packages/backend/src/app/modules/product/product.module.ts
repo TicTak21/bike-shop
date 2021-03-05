967c2ca0e69664ec6a11a25c4b1ad581c8ca93ad
Alex <tictak211@gmail.com>
2021-03-05 18:27:06 +0200

    refactor(backend): refactor product module

--- packages/backend/src/app/modules/product/product.module.ts
+++ packages/backend/src/app/modules/product/product.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DbModule } from '../db/db.module';
 import { ProductResolver } from './product.resolver';
-import { ProductService } from './product.service';
 
 @Module({
   imports: [DbModule],
-  providers: [ProductService, ProductResolver],
+  providers: [ProductResolver],
 })
 export class ProductModule {}
