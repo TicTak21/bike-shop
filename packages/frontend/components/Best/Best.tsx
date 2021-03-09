aed59fc534861f91ee33ccda2869c05d2e4ee2af
Alex <tictak211@gmail.com>
2021-03-09 17:45:44 +0200

    feat(frontend): Best products section done

--- packages/frontend/components/Best/Best.tsx
+++ packages/frontend/components/Best/Best.tsx
@@ -1,6 +1,6 @@
 import Title from '../Title';
 import Layout from '../Layout';
-import { Box, Flex, Text, IconButton } from '@chakra-ui/react';
+import { Box, Flex, Text, IconButton, Grid, SimpleGrid } from '@chakra-ui/react';
 import Image from 'next/image';
 import { RightArrowIcon } from '../../public/icons/Arrow/RightArrow';
 import Link from '../Link';
@@ -8,8 +8,9 @@ import Link from '../Link';
 const Best = () => {
   const data = {
     big: {
-      title: 'streetster bakerstreet',
+      name: 'streetster bakerstreet',
       desc: 'скорость соедененная с удобством',
+      image: '/img/Best/Best-Big-1.png',
     },
     small: [
       { image: '/img/Best/Best-Small-1.png', name: 'Шлем Octane' },
@@ -19,79 +20,86 @@ const Best = () => {
     ],
   };
 
-  const GridFlexBasis = ['100%', , , 'calc(50% - 2rem)'];
-
   return (
     <Box as="section">
       <Layout>
         <Title color="black">Избранные товары</Title>
+        <SimpleGrid
+          gridGap={4}
+          columns={[1, , , 2]}
+          justifyContent="space-between"
+          mt={8}
+          h="450px"
+        >
+          <Box
+            position="relative"
+            bgImage={`url(${data.big.image})`}
+            bgRepeat="no-repeat"
+            bgSize="cover"
+            bgPosition="center"
+            h="inherit"
+            transition="0.3s ease-in-out"
+            _hover={{ transform: 'scale(1.03)' }}
+          >
+            <Box
+              position="absolute"
+              w="100%"
+              top="0"
+              left="0"
+              color="white"
+              p={6}
+              zIndex={2}
+              textAlign="right"
+            >
+              <Text textTransform="uppercase" fontWeight="700" fontSize="2xl">
+                {data.big.name}
+              </Text>
+              <Text>{data.big.desc}</Text>
+            </Box>
 
-        {/* <Flex flexWrap="wrap" alignItems="flex-start" mt={8} gridGap={6}>
-          <Box flexBasis={GridFlexBasis}>
-            <Link href={`/${data.big.title}`}>
-              <Box position="relative">
-                <Box
-                  position="absolute"
-                  w="100%"
-                  top="0"
-                  left="0"
+            <Box
+              position="absolute"
+              w="100%"
+              top="60%"
+              left="0"
+              color="white"
+              p={6}
+              zIndex={2}
+              textAlign="right"
+            >
+              <Link href={`/${data.big.name}`}>
+                <IconButton
                   color="white"
-                  p={6}
-                  zIndex={2}
-                  textAlign="right"
-                >
-                  <Text textTransform="uppercase" fontWeight="700" fontSize="2xl">
-                    {data.big.title}
-                  </Text>
-                  <Text>{data.big.desc}</Text>
-                </Box>
-
-                <Box
-                  position="absolute"
-                  w="100%"
-                  top="60%"
-                  left="0"
-                  color="white"
-                  p={6}
-                  zIndex={2}
-                  textAlign="right"
-                >
-                  <IconButton
-                    color="white"
-                    variant="link"
-                    aria-label="Right arrow"
-                    icon={<RightArrowIcon boxSize={6} />}
-                    _hover={{ transform: 'scale(1.2)' }}
-                  />
-                </Box>
-
-                <Box>
-                  <Image src="/img/Best/Best-Big-1.png" width="750px" height="450px" />
-                </Box>
-              </Box>
-            </Link>
+                  variant="link"
+                  aria-label="Right arrow"
+                  icon={<RightArrowIcon boxSize={6} />}
+                  _hover={{ transform: 'scale(1.2)' }}
+                />
+              </Link>
+            </Box>
           </Box>
 
-          <Flex flexBasis={GridFlexBasis} flexDirection="column" gridGap={4}>
+          <SimpleGrid columns={2} spacing={4}>
             {data.small.map((el, i) => (
-              <Flex
-                key={i}
-                flexDirection="column"
-                flexBasis="100%"
-                px={8}
-                py={4}
-                bg="black"
-                justifyContent="center"
-                alignItems="center"
-              >
-                <Image src={el.image} width="100px" height="100px" />
-                <Text color="white" fontSize="lg">
-                  {el.name}
-                </Text>
-              </Flex>
+              <Link key={i} href={`/${el.name}`} _hover={{ transform: 'scale(1.03)' }}>
+                <Flex
+                  bg="#151515"
+                  height="100%"
+                  justifyContent="center"
+                  alignItems="center"
+                  flexDirection="column"
+                  gridGap={2}
+                  p={2}
+                >
+                  <Image src={el.image} width="100px" height="100px" />
+                  <Text color="white" isTruncated maxW="150px">
+                    {el.name}
+                  </Text>
+                </Flex>
+              </Link>
             ))}
-          </Flex>
-        </Flex> */}
+          </SimpleGrid>
+        </SimpleGrid>
       </Layout>
     </Box>
   );
