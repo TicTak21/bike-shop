31a307c5af16f581b901675036cfce6fc98b8f2f
Alex <tictak211@gmail.com>
2021-03-09 21:05:25 +0200

    feat(frontend): logo customization(color)

--- packages/frontend/components/Logo.tsx
+++ packages/frontend/components/Logo.tsx
@@ -1,11 +1,17 @@
 import { Image } from '@chakra-ui/react';
 
 export interface LogoProps {
-  imgSrc?: string;
+  variant?: 'black' | 'white';
 }
 
-const Logo: React.FC<LogoProps> = ({ imgSrc }) => {
-  return <Image src={imgSrc || '/icons/Logo/logo.svg'} />;
+const Logo: React.FC<LogoProps> = ({ variant = 'white' }) => {
+  // variant for css `invert` function
+  const variantsTable = {
+    white: 0,
+    black: 1,
+  };
+
+  return <Image src="/icons/Logo/logo.svg" filter={`invert(${variantsTable[variant]})`} />;
 };
 
 export default Logo;
