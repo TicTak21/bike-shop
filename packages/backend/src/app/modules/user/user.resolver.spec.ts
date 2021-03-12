506d7079b27bb15a360d9f084220b14c7e4da884
Alex <tictak211@gmail.com>
2021-03-12 20:47:04 +0200

    feat(backend): add tests for user.resolver

--- packages/backend/src/app/modules/user/user.resolver.spec.ts
+++ packages/backend/src/app/modules/user/user.resolver.spec.ts
@@ -1,9 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DbModule } from '../db/db.module';
+import { CreateUserInput } from './dto/create-user.input';
+import { UpdateUserInput } from './dto/update-user.input';
 import { UserResolver } from './user.resolver';
 
 describe('UserResolver', () => {
   let resolver: UserResolver;
+  const mockUser: CreateUserInput = {
+    username: 'test_user',
+    email: 'test_user@mail.com',
+    password: 'qwer_1234',
+    role: 'Client',
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -19,14 +27,37 @@ describe('UserResolver', () => {
   });
 
   it('should return null from `user` if user not found', async () => {
-    const res = resolver.user('1234');
+    const user = resolver.user('1234');
 
-    await expect(res).resolves.toBe(null);
+    await expect(user).resolves.toBe(null);
   });
 
   it('should return array from `users`', async () => {
-    const res = resolver.users();
+    const users = await resolver.users();
 
-    expect(res).resolves.toBeCalledWith(expect.anything());
+    expect(Array.isArray(users)).toBe(true);
+  });
+
+  it('should create user via `createUser`', async () => {
+    const user = await resolver.createUser(mockUser);
+
+    expect(user.email).toEqual(mockUser.email);
+
+    await resolver.deleteUser(user.userId);
+  });
+
+  it('should update user via `updateUser`', async () => {
+    const user = await resolver.createUser(mockUser);
+
+    const mockUpdateUser: UpdateUserInput = {
+      email: 'update_test_user@mail.com',
+      passwordHash: '',
+    };
+
+    const updatedUser = await resolver.updateUser(user.userId, mockUpdateUser);
+
+    expect(updatedUser.email).toEqual(mockUpdateUser.email);
+
+    await resolver.deleteUser(user.userId);
   });
 });
